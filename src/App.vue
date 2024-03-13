7c10f9caf57cd8bea5fc14c60e2b4c4615eed8a2
肖波 <xiaoboc@dcits.com>
2024-03-13 16:25:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import '@ar-js-org/ar.js'
+</script>
+
+<template>
+  <a-scene embedded arjs>
+    <a-marker preset="hiro">
+      <!-- we use cors proxy to avoid cross-origin problems ATTENTION! you need to set up your server -->
+      <a-entity position="0 0 0" scale="0.05 0.05 0.05" gltf-model="./src/assets/scene.gltf"></a-entity>
+    </a-marker>
+    <a-entity camera></a-entity>
+  </a-scene>
+</template>
